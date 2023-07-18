396e34e89801dba5fa805437bbbd40c725fdb44a
xuwei <xuwei05@inspur.com>
2023-07-18 16:18:33 +0800

    dbus code

---
+++ dbus-1.13.20/out/build/x64-Debug/CMakeFiles/CheckIncludeFiles/HAVE_SYS_EVENT_H.c
@@ -0,0 +1,8 @@
+/* */
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/event.h>
+
+
+int main(void){return 0;}
+
---
+++ dbus-1.13.20/out/build/x64-Debug/dbus-sln.bat
@@ -0,0 +1,2 @@
+set PATH=D:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin;%PATH%
+start dbus.sln
